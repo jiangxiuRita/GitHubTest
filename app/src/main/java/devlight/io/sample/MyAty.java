eade21e39445c91a5836ee39fae393d28868a870
Lenovo <2735749668@qq.com>
2019-05-28 21:36:28 +0800

    第一次提交（模板）

---
+++ app/src/main/java/devlight/io/sample/MyAty.java
@@ -0,0 +1,13 @@
+package devlight.io.sample;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class MyAty extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
